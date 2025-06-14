4d8911bd65d4261b16705b8f6a11d4101cb8cb78
pagislan <pegislan@gmail.com>
2025-06-14 12:58:35 -0400

    Sergio hola munxdo

---
+++ holamundosergio.java
@@ -0,0 +1,12 @@
+package holamundo;
+
+/**
+ * Autor: Alvarado
+ * Proyecto: Hola Mundo en NetBeans
+ */
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo desde Java en NetBeans!");
+    }
+}
